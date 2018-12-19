7cca5ec1fd2342ba047d7a6aca3846b63fec5891
Jarek <spsp01@o2.pl>
2018-12-19 09:52:19 +0100

    First spider

--- spid/spiders/brw.py
+++ spid/spiders/brw.py
@@ -4,10 +4,12 @@ from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
 
+
+
 class MoreleSpider(CrawlSpider):
     name = 'brw'
     allowed_domains = ['brw.pl']
-    start_urls = ['https://www.brw.pl/mebloscianka-fever-3,24928/']
+    start_urls = ['https://www.brw.pl']
 
     rules = (
         Rule(LinkExtractor(), callback='parse_item', follow=True),
@@ -15,6 +17,16 @@ class MoreleSpider(CrawlSpider):
 
     def parse_item(self, response):
         price = response.css('div[class=cena] *::text').extract_first().rstrip().replace('\r\n\t\t\t\t\t\t\t\t\t\t\t','').replace(',-','')
-        #if price !=[]:
-        print(response.url)
-        print(price)
+        if price !=[]:
+            old_price=response.xpath('//span[@class="old-price stara-cena"]/text()').extract_first().replace(',-','')
+            title = response.xpath('//title/text()').extract_first()
+            h1 = response.xpath('//h1/text()').extract()[-1]
+            category = response.xpath('//span[@itemprop="name"]/text()').extract()
+            print(response.url)
+            print(old_price,' ',price, ' ',title,' ',category)
+
+    def categrystring(self):
+        category =['BRW', 'meblościanki', 'nowoczesne']
+
+
+
--- spid/spiders/prices_morele.py
+++ spid/spiders/prices_morele.py
@@ -1,32 +1,21 @@
-# -*- coding: utf-8 -*-
-import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors.lxmlhtml import LxmlLinkExtractor
+from scrapy.item import Item, Field
+
+class MyItem(Item):
+    url= Field()
 
 
-# class PricesMoreleSpider(scrapy.Spider):
-#     name = 'prices_morele'
-#     start_urls = ['https://www.morele.net/']
-#
-#     def parse(self, response):
-#         self.valid_url.append({'url': response.url,
-#                                'from': from_url,
-#                                'text': from_text})
-#
-#         print(response)
+class someSpider(CrawlSpider):
+    name = 'crawltest'
+    allowed_domains = ['axa.pl']
+    start_urls = ['https://axa.pl/']
+    rules = (Rule(LxmlLinkExtractor(allow=()), callback='parse_obj', follow=True),)
 
-class ToScrapeCSSSpider(scrapy.Spider):
-    name = "toscrape-css"
-    start_urls = [
-        'http://quotes.toscrape.com/',
-    ]
-
-    def parse(self, response):
-        for quote in response.css("div.quote"):
-            yield {
-                'text': quote.css("span.text::text").extract_first(),
-                'author': quote.css("small.author::text").extract_first(),
-                'tags': quote.css("div.tags > a.tag::text").extract()
-            }
-
-        next_page_url = response.css("li.next > a::attr(href)").extract_first()
-        if next_page_url is not None:
-            yield scrapy.Request(response.urljoin(next_page_url))
+    def parse_obj(self,response):
+        item = MyItem()
+        item['url'] = []
+        print(response)
+        for link in LxmlLinkExtractor(allow=(),deny = self.allowed_domains).extract_links(response):
+            item['url'].append(link.url)
+        return item
